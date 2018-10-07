0f86be2c3dae21936d0f8ef6e3208f4fcc85d2e1
AZn5ReD <AZn5ReD>
2018-10-08 00:40:22 +0200

    Initial

---
+++ src/BotRoC.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BotRoC.ClassLib;
+
+namespace BotRoC.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdbServer server = new AdbServer();
+            int res = server.StartServer();
+            server.GetScreenSize();
+        }
+    }
+}
